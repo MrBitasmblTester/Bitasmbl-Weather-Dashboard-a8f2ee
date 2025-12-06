f5bcb1d1b0cc0a6e07cc85eab8af0d7281ce562d
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-12-06 12:49:42 +0400

    Add ForecastPoint.java

---
+++ java-service/src/main/java/com/example/weather/model/ForecastPoint.java
@@ -0,0 +1,8 @@
+package com.example.weather.model;
+
+public class ForecastPoint {
+    private String time;
+    private double temperature;
+    private double humidity;
+    // getters/setters
+}
